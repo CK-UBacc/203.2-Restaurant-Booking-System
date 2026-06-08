83f027d9006651468b4c127880f48af150e91924
CK-UBacc <270708756@yoobeestudent.ac.nz>
2026-06-08 13:58:06 +1200

    Update app.py
    Dummy commit for demonstration

--- app.py
+++ app.py
@@ -1,14 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 import sqlite3
+import os
 
 app = Flask(__name__)
 
-connection = sqlite3.connect("database.db")
-cursor = connection.cursor
+def createDatabase():
+    '''
+    I don't know what this does yet but I'll write a propper thing when its done
+
+    '''
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor
 
 @app.route("/")
 def index():
-    return "It works!"
+    '''
+    Route to home page.
+
+    Args:
+    None
+
+    Returns:
+    render_template: template for the home page
+
+    '''
+    return render_template('index.html')
+
+@app.route("/booking/<int:id>", methods=["GET", "POST"]) #NEEDS: HTML page
+def booking(id):
+    '''
+    Route to booking form.
+    Booking form is ment to be an embed on a page of another website so there will be no route to the booking form from our web page.
+
+    Args:
+    id (int): restaurant ID
+
+    Returns:
+    render_template: template for the booking form
+
+    '''
+    return render_template()
 
 if __name__ == "__main__":
     app.run(debug=True)
