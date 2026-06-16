a54fc60690ae4e59448a9acfd248d9e9181a9c77
ByronR02 <270343943@yoobeestudent.ac.nz>
2026-06-16 14:15:56 +1200

    Update app.py

--- app.py
+++ app.py
@@ -221,6 +221,18 @@ def dashboard():
     tables = Table.query.all()
     return render_template("dashboardOverview.html", bookings=bookings, tables=tables, active="overview")
 
+@app.route("/loginpage", methods=["GET", "POST"])
+def loginpage():
+    if request.method == "POST":
+        pass
+
+    return render_template("loginpage.html")
+
+
+
+
+
+
 @app.route("/dashboard/bookings", methods=["GET"])
 def dashboardBookings():
     '''
