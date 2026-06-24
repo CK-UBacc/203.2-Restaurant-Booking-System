6beae1c2c6029d83ec46bc9da68b67715358176a
ByronR02 <270343943@yoobeestudent.ac.nz>
2026-06-24 23:03:46 +1200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, url_for, request, flash, ses
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
 from flask_mail import Mail, Message
-from wtformsTesting import TestBookingForm
+
 # import os # not currently being used?
 import datetime
 import calendar as cal_module
