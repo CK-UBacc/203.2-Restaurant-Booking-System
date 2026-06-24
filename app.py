229216aa2c7b05064087104b1b576d40aa3f1faa
CK-UBacc <270708756@yoobeestudent.ac.nz>
2026-06-24 23:10:46 +1200

    Late night crunch
    -removed "dashboard.html" as it was not being used
    -removed wtrformsTesting import agian
    - renamed "/dashboard/tables" route to "/dashboard/restaurant-info" to better reflect the pages purpose
    - changed the "manage tables"  link text on dashboardBase to "manage restaurant info"
    - removed the table status key on the restaurant-info page
    - removed table occupation status from the table boxes on restaurant-info
    - removed occupation status indicator on table management info box popup

--- app.py
+++ app.py
@@ -2,11 +2,9 @@ from flask import Flask, render_template, redirect, url_for, request, flash, ses
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
 from flask_mail import Mail, Message
-from wtformsTesting import TestBookingForm
 # import os # not currently being used?
 import datetime
 import calendar as cal_module
-# from wtformsTesting import TestBookingForm
 
 app = Flask(__name__)
 
@@ -517,7 +515,7 @@ def dashboardBookingsEdit(id):
         flash("Failed to update booking.", "error")
     return redirect(url_for("dashboardBookings"))
 
-@app.route("/dashboard/tables", methods=["GET", "POST"])
+@app.route("/dashboard/restaurant-info", methods=["GET", "POST"])
 def dashboardTables():
 
     if not session.get("logged_in"):
